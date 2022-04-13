bb9a0cbac0dc43720358d8a2948a45a48f5c0dab
Cristian Stefanescu <me@linux365.ro>
2022-04-13 15:05:08 +0300

    enable envs
    add env variables for docker

--- src/conn.php
+++ src/conn.php
@@ -1,14 +1,14 @@
 <?php
-//connection to mysql in one file
 
-$servername = "localhost";
-$username = "username";
-$password = "password";
-$database = "database";
-// Create connection
-$conn = new mysqli($servername, $username, $password, $database);
-// Check connection
-if ($conn->connect_error) {
-  die("Connection failed: " . $conn->connect_error);
+define('DB_USERNAME',getenv('MYSQL_USERNAME'));
+define('DB_PASSWORD',getenv('MYSQL_PASSWORD'));
+define('DB_HOST',getenv('MYSQL_HOST'));
+define('DB_PORT',getenv('MYSQL_PORT'));
+define('DB_DATABASE',getenv('MYSQL_DATABASE'));
+
+$con = mysqli_connect(DB_HOST,DB_USERNAME,DB_PASSWORD,DB_DATABASE);
+if (mysqli_connect_errno())
+  {
+  echo "Failed to connect to MySQL: " . mysqli_connect_error();
 }
 ?>
